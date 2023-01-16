5ae41f55bb0988d0eb8938df8e7ab28eb54a2ff7
gabriel <batistutag190@gmail.com>
2023-01-16 20:52:24 -0300

    add: lamp-xampp.sh (script for install the lamp and xampp)

---
+++ backup/php/lamp-xampp.sh
@@ -0,0 +1,11 @@
+username=whoami
+
+sudo apt update
+
+sudo apt-get install lamp-server^ -y
+
+cd /home/$username/Downloads
+
+chmod +x xampp-linux-x64-8.2.0-0-installer.run
+
+sudo ./xampp-linux-x64-8.2.0-0-installer.run
