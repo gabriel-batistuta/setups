9af4b4374a9b98cf90496779be526cccd07a0fbb
gabriel-batistuta <batistutag190@gmail.com>
2023-12-25 18:59:35 -0300

    ajustado path de git clone do repo do oh my zsh

--- zsh/oh-my-zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh/oh-my-zsh.sh
@@ -5,8 +5,8 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 # plugin: zsh-syntax-highlighting
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
 # source: https://github.com/ohmyzsh/ohmyzsh/issues/7688#issuecomment-476947050 
-git clone zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # plugin fzf
 # source: https://github.com/junegunn/fzf
