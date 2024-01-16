b14ae86d7d95113c786097ed6fb84316bf4b2618
gabriel-batistuta <batistutag190@gmail.com>
2024-01-16 19:47:51 -0300

    feat: add test.sh to gitignore, feat: add latest download and setter for node binary

--- development/node/node.sh
+++ development/node/node.sh
@@ -3,3 +3,10 @@ sudo snap install node --classic
 sudo apt install npm -y
 
 sudo apt install yarn -y
+
+node_source_code='https://nodejs.org/dist/v20.11.0/node-v20.11.0.tar.gz'
+
+cd ~/Documents/temporary && wget $node_source_code 
+tar xzf node-v20.11.0
+sudo mv node-v20.11.0.tar.gz/bin/node /usr/bin
+echo "export node='/usr/bin/node'" >> ~./zshrc
