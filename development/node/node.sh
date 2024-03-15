09407af49849ed5b12820e81a747883938fbc6b6
gabriel <batistutag190@gmail.com>
2024-03-15 13:32:24 -0300

    mudada configuração de instalação do node

--- development/node/node.sh
+++ development/node/node.sh
@@ -1,14 +1,1 @@
-node_source_code='https://nodejs.org/dist/v20.11.0/node-v20.11.0.tar.gz'
-
-cd ~/Documents/temporary && wget $node_source_code 
-tar xzf node-v20.11.0
-sudo mv node-v20.11.0.tar.gz/bin/node /usr/bin
-# echo "export node='/usr/bin/node'" >> ~./zshrc
-echo "export PATH=$PATH:/usr/bin/node" >> ~/.zshrc
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-
-sudo ln -sf /usr/local/n/versions/node/20.11.0/bin/node /usr/bin/node
-
-sudo npm install npm -g
+sudo snap install node --classic
