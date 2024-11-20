f44ecdbef3ca981afe00f3f2256ee3cbd6b42015
Gabriel <batistutag190@gmail.com>
2024-11-20 13:24:36 -0300

    add change of firefox version normal to developer

--- requirements/setup.sh
+++ requirements/setup.sh
@@ -1,11 +1,18 @@
-# firefox
+# firefox developer
 wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=pt-BR" -O firefox-developer.tar.bz2
 sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
 sudo mv /opt/firefox*/ /opt/firefox-developer
 sudo ln -sf /opt/firefox-developer/firefox /usr/bin/firefox-developer
 echo -e '[Desktop Entry]\n Version=yy.y.y\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
 sudo chmod +x /usr/share/applications/firefox-developer.desktop
-sudo update-alternatives --config x-www-browser
+sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/firefox-developer 60
+# sudo update-alternatives --config x-www-browser
+sudo update-alternatives --set x-www-browser /usr/bin/firefox-developer
+update-alternatives --display x-www-browser
+
+# desinstalar firefox padrão
+sudo apt remove --purge firefox
+
 
 # git
 sudo apt-get install git
