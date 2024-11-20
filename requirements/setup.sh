772581c379bbe40d64c08f5a37adeb455191a9c3
Gabriel <batistutag190@gmail.com>
2024-11-20 13:04:03 -0300

    update firefox-developer

--- requirements/setup.sh
+++ requirements/setup.sh
@@ -1,3 +1,12 @@
+# firefox
+wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=pt-BR" -O firefox-developer.tar.bz2
+sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
+sudo mv /opt/firefox*/ /opt/firefox-developer
+sudo ln -sf /opt/firefox-developer/firefox /usr/bin/firefox-developer
+echo -e '[Desktop Entry]\n Version=yy.y.y\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
+sudo chmod +x /usr/share/applications/firefox-developer.desktop
+sudo update-alternatives --config x-www-browser
+
 # git
 sudo apt-get install git
 git config --global user.email batistutag190@gmail.com
