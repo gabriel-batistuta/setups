6c7aaac782691ee7cc9b6071273e4e571cdf4e89
Gabriel <batistutag190@gmail.com>
2024-12-01 10:47:03 -0300

    add comment in install of brew

--- archsetup.sh
+++ archsetup.sh
@@ -181,4 +181,4 @@ echo 'alias difusion="cd ~/easy-diffusion && ./start.sh"' >> ~/.zshrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+echo "#eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
