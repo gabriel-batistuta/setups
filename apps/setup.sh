318296dd33f30126cffee11deae4db08b05b9379
gabriel <batistutag190@gmail.com>
2025-07-06 22:02:05 -0300

    update telegram app download

--- apps/setup.sh
+++ apps/setup.sh
@@ -1,6 +1,3 @@
-# install the Authy Desktop in:'https://snapcraft.io/authy' for generate token for login (GitHub, Twitch)
-sudo snap install authy
-
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub "https://flathub.org/repo/flathub.flatpakrepo"
 
@@ -11,7 +8,6 @@ flatpak install flathub com.obsproject.Studio
 # sudo ubuntu-drivers autoinstall  # (caso esteja usando Ubuntu)
 # reboot
 
-
 flatpak install flathub com.discordapp.Discord
 
 flatpak install flathub net.lutris.Lutris
@@ -19,13 +15,9 @@ flatpak install flathub net.lutris.Lutris
 # qbitorrent (downloader and seeder of archives in web)
 sudo apt install qbittorrent -y
 
-mkdir ~/Documents/temporary && cd ~/Documents/temporary 
-wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
-
-sudo dpkg -i steam.deb
-sudo apt-get install -f
+mkdir ~/Documentos/temporary && cd ~/Documentos/temporary 
 
 # telegram desktop (social network for phones)
-sudo apt install telegram-desktop -y
+sudo snap install telegram-desktop -y
 
 sudo snap install vidcutter
